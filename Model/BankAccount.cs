10cbb12f5a9550c09e8437d385782da60bf4045f
lisacasio <neonwave2702@gmail.com>
2024-10-08 16:55:00 +0300

    Addet Model Addet View

--- Model/BankAccount.cs
+++ Model/BankAccount.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace BankAccMVC.Model
 {
     internal class BankAccount
-    {
+    {    
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Amount { get; set; }
+
+            public BankAccount(int id, string name,   decimal amount)
+            {
+                Id = id;
+                Name = name;
+                Amount = amount;
+            }
     }
 }
